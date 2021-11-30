1cb726610f44bba20ec11dfddcc593ca700f7abc
ecknight <ellyknight@gmail.com>
2021-11-30 11:09:37 -0700

    Tidying detection categories

--- Score.R
+++ Score.R
@@ -7,22 +7,17 @@ library(AICcmodavg)
 dat <- readr::read_tsv("RecognizerResults_Validated.txt", 
                        col_names = c("Filename", "TimeOffset", "Duration", "Level", "Quality", "Score", "Recognizer", "Comments"),
                        col_types = "ccddddcd") %>% 
-  mutate(File = str_sub(Filename, -34, -1))
-
-dat.1 <- dat %>% 
-  dplyr::filter(Comments==1)
-
-dat.0 <- dat %>% 
-  dplyr::filter(Comments==0)
+  mutate(File = str_sub(Filename, -34, -1),
+         Detection = ifelse(Comments==0, "FP", "TP"))
 
 #2. Plot----
 ggplot(dat) +
   geom_point(aes(x=Score, y=Level)) +
-  facet_wrap(~Comments)
+  facet_wrap(~Detection)
 
 #3. Test----
-lm.int <- lmer(Score ~ Level*factor(Comments) + (1|File), data=dat)
-lm.add <- lmer(Score ~ Level + factor(Comments) + (1|File), data=dat)
-lm.level <- lmer(Score ~ Level + (1|File), data=dat)
-lm.null <- lmer(Score ~ 1 + (1|File), data=dat)
+lm.int <- lmer(Score ~ Level*factor(Detection) + (1|File), data=dat, REML=FALSE)
+lm.add <- lmer(Score ~ Level + factor(Detection) + (1|File), data=dat, REML=FALSE)
+lm.level <- lmer(Score ~ Level + (1|File), data=dat, REML=FALSE)
+lm.null <- lmer(Score ~ 1 + (1|File), data=dat, REML=FALSE)
 aictab(list("interaction" = lm.int, "additive" = lm.add, "levelonly" = lm.level, "null" = lm.null))
